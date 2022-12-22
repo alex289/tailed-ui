4f3a8c384e2849646e1997a8d8d2ca3a00a4c7cb
alex289 <mccr4fthd@gmail.com>
2022-12-22 01:31:29 +0100

    fix: Storybook deployment

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,7 +2,6 @@ module.exports = {
   stories: ['../src/**/*.stories.tsx'],
   addons: [
     '@storybook/addon-actions',
-    '@storybook/addon-links',
     'storybook-dark-mode',
     {
       name: '@storybook/addon-postcss',
