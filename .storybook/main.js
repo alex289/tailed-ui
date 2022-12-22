19e9b85be25830b7b070be3f732179c7a4223177
alex289 <mccr4fthd@gmail.com>
2022-12-22 01:42:16 +0100

    fix: Try fixing storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,12 @@
 module.exports = {
   stories: ['../src/**/*.stories.tsx'],
+  framework: '@storybook/react',
+  core: {
+    builder: 'webpack5',
+  },
   addons: [
     '@storybook/addon-actions',
+    '@storybook/addon-essentials',
     'storybook-dark-mode',
     {
       name: '@storybook/addon-postcss',
